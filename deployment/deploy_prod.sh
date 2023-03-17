a949efb3ce38f5a8712244049d6ef1daa95f2cc6
Muhammad Rashid <rashiddaha80@gmail.com>
2023-03-17 14:39:05 +0500

    up

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -6,7 +6,7 @@ ssh root@143.110.240.81 <<EOF
   source /opt/envs/drfblogproject/bin/activate
   pip install -r requirements.txt
   ./manage.py makemigrations
-  ./manage.py migrate 
+  ./manage.py migrate  --run-syncdb
   sudo service gunicorn restart
   sudo service nginx restart
   exit
